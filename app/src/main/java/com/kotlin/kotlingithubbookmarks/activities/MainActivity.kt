5a25ce9940840d9a242e8f000e6aabbcff9a4898
Samson <samnjoroge6035@gmail.com>
2021-09-01 13:01:56 +0300

    Initial commit

---
+++ app/src/main/java/com/kotlin/kotlingithubbookmarks/activities/MainActivity.kt
@@ -0,0 +1,92 @@
+package com.kotlin.kotlingithubbookmarks.activities
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.textfield.TextInputLayout
+import com.kotlin.kotlingithubbookmarks.R
+import com.kotlin.kotlingithubbookmarks.app.Constants
+import kotlinx.android.synthetic.main.activity_display.*
+
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private val TAG: String = MainActivity::class.java.simpleName
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        /**
+        //TextView.text points to CharSequence object
+        val str1 = txvGreeting.text
+        txvGreeting.text = "Welcome"
+
+        //EditText.text points to Editable object
+        val str2 = etName.text.toString()
+        etName.setText("Samson")
+         **/
+
+        //setSupportActionBar(toolbar)
+
+
+    }
+
+    /** save app username in SharedPreferences */
+    fun saveName (view: View ) {
+
+        if (isNotEmpty(etName,inputLayoutName)) {
+            val personName = etName.text.toString()
+            val sp = getSharedPreferences(Constants.APP_SHARED_PREFERENCES, Context.MODE_PRIVATE)
+            val editor = sp.edit()
+            editor.putString(Constants.KEY_PERSON_NAME, personName)
+            editor.apply()
+        }
+    }
+
+    /** Search repositories on github after passing data to DisplayActivity */
+    fun listRepositories(view: View){
+
+        if (isNotEmpty(etRepoName,inputLayoutRepoName)){
+            val queryRepo = etRepoName.text.toString()
+            val repoLanguage = etLanguage.text.toString()
+
+            val intent = Intent(this@MainActivity, DisplayActivity::class.java)
+            intent.putExtra(Constants.KEY_QUERY_TYPE, Constants.SEARCH_BY_REPO)
+            intent.putExtra(Constants.KEY_REPO_SEARCH, queryRepo)
+            intent.putExtra(Constants.KEY_LANGUAGE, repoLanguage)
+            startActivity(intent)
+        }
+    }
+
+    /**Search repositories of a particular github user after passing data to DisplayActivity */
+    fun listUserRepositories(view: View){
+
+        if (isNotEmpty(etGithubUser,inputLayoutGithubUser)) {
+            val githubUser = etGithubUser.text.toString()
+
+            val intent = Intent(this, DisplayActivity::class.java)
+            intent.putExtra(Constants.KEY_QUERY_TYPE, Constants.SEARCH_BY_USER)
+            intent.putExtra(Constants.KEY_GITHUB_USER, githubUser)
+            startActivity(intent)
+        }
+    }
+
+    fun isNotEmpty(editText: EditText, textInputLayout: TextInputLayout):Boolean {
+
+        if (editText.text.toString().isEmpty()){
+            textInputLayout.error = "Cannot be blank !"
+            return false
+        } else{
+            textInputLayout.isErrorEnabled = false
+            return true
+        }
+    }
+
+}
---
+++ app/src/main/java/com/kotlin/kotlingithubbookmarks/models/ErrorResponse.java
@@ -0,0 +1,11 @@
+package com.kotlin.kotlingithubbookmarks.models;
+
+
+public class ErrorResponse {
+
+	private String message;
+
+	public String getMessage() {
+		return message;
+	}
+}
